80c67764f0241d07386cb3b74fadd7370ea6285b
Michal Jurosz <mj@mj41.cz>
2014-04-27 14:53:27 +0200

    Add methods for debugging

--- lib/Perl6/Analytics/Projects.pm
+++ lib/Perl6/Analytics/Projects.pm
@@ -25,6 +25,34 @@ sub projects_final_fpath {
 	return 'data/projects-final.json';
 }
 
+sub get_ipos_str {
+	my ( $self, $offset ) = @_;
+	$offset //= 0;
+	my $line = (caller(1+$offset))[2] || 'l';
+	my $sub = (caller(2+$offset))[3] || 's';
+	return "$sub($line)";
+}
+
+sub dump {
+	croak "Missing parameter for 'dump'.\n" if scalar @_ < 3;
+	my ( $self, $text, $struct, $offset ) = @_;
+	unless ( $self->{dumper_loaded} ) {
+		require Data::Dumper;
+		$self->{dumper_loaded} = 1;
+	};
+
+	local $Data::Dumper::Indent = 1;
+	local $Data::Dumper::Pad = '';
+	local $Data::Dumper::Terse = 1;
+	local $Data::Dumper::Sortkeys = 1;
+	local $Data::Dumper::Deparse = 1;
+	unless ( $text ) {
+		print Data::Dumper->Dump( [ $struct ] );
+		return;
+	}
+	print $text . ' on ' . $self->get_ipos_str($offset) . ': ' . Data::Dumper->Dump( [ $struct ] );
+}
+
 sub load_base_list {
 	my ( $self ) = @_;
 
