b604554e1d41115114456bfcf31e647aa93e240d
Michal Jurosz <mj@mj41.cz>
2014-05-10 22:54:50 +0200

    Extra arguments to Git::ClonesManager object construction

--- lib/Perl6/Analytics/Role/ClonesManager.pm
+++ lib/Perl6/Analytics/Role/ClonesManager.pm
@@ -5,10 +5,16 @@ use warnings;
 
 use Git::ClonesManager;
 
+sub gcm_obj_extra_args {
+	return {};
+}
+
 sub gcm_obj {
 	my ( $self ) = @_;
-	$self->{addons}{gcm_obj} = Git::ClonesManager->new( verbose_level => $self->{vl} )
-		unless $self->{addons}{gcm_obj};
+	$self->{addons}{gcm_obj} = Git::ClonesManager->new(
+		verbose_level => $self->{vl},
+		%{ $self->gcm_obj_extra_args() }
+	) unless $self->{addons}{gcm_obj};
 	return $self->{addons}{gcm_obj};
 }
 
